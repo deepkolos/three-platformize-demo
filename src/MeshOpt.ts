1088d2e2ad9c3ed0e0076452b62cfdd79f4250a5
deepkolos <deepkolos@qq.com>
2021-02-16 11:34:27 +0800

    feat: add MeshoptDecoder asm test

--- src/MeshOpt.ts
+++ src/MeshOpt.ts
@@ -1,16 +1,26 @@
 import { Demo } from './Demo';
 import { GLTF } from 'three-platformize/examples/jsm/loaders/GLTFLoader';
 import { AmbientLight, Color, PointLight } from 'three-platformize';
-import { MeshoptDecoder } from 'three-platformize/examples/jsm/libs/meshopt_decoder.module';
+// import { MeshoptDecoder } from 'three-platformize/examples/jsm/libs/meshopt_decoder.module';
+import { MeshoptDecoder } from 'three-platformize/tools/meshopt_decoder.asm.module';
+
+/**
+ * 测试手机：小米8
+ * 
+ * 加载时间ms
+ *  wasm 58  61  62
+ *  asm  144 139 134
+ */
 
 export class DemoMeshOpt extends Demo {
   async init(): Promise<void> {
     const { gltfLoader, camera, scene } = this.deps;
     gltfLoader.setMeshoptDecoder(MeshoptDecoder);
-
+    const t = Date.now()
     const gltf = (await gltfLoader.loadAsync(
       'https://meshoptimizer.org/demo/pirate.glb',
     )) as GLTF;
+    console.log(Date.now() - t);
 
     const pointLight = new PointLight(0xffffff, 0.8);
     pointLight.position.set(3, 3, 0);
